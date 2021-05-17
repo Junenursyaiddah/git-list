d9ccafc765d7730db0431a7648343a13b1b9a51d
June nursyaiddah <missjunestyle@gmail.com>
2021-05-18 04:57:12 +0700

    test-axio-api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React from 'react';
 import Results from "./components/Results";
 import SearchBar from "./components/SearchBar";
 
 
-function App() {
+const App = () => {
   return (
-    <div>
-       <h2>Repo search</h2>
+      <div>
+      <h2>Repo search</h2>
       <SearchBar/>
       <Results/>
-    </div>
-   
+     </div>
   );
 }
+
+
+
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,27 @@
 import React,  { useState } from "react";
+import axios from 'axios'
 
 const SearchBar = () => {
     const [searchInput, setSearchInput] = useState('')
+    const [repos, setRepos] = useState([]);
     
     const handleChange = (e) => {
         setSearchInput(e.target.value);
     };
 
-    const handleClick = () => {
+    const handleClick = async () => {
         console.log(searchInput)
-    }
+
+        try{
+        const result = await axios (`https://api.github.com/users/${searchInput}`)
+
+        setRepos(result);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    console.log(repos);
 
     
     return (
